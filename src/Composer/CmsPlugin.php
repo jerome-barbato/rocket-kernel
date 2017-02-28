71b58c5e70f3968ff21e88531ffd805fbe56c270
Paul Coudeville <paul@coudeville.info>
2017-02-28 12:30:08 +0100

    Composer CMS Install for "wordpress-core"

--- src/Composer/CmsPlugin.php
+++ src/Composer/CmsPlugin.php
@@ -9,7 +9,20 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
+/**
+ * Class CmsPlugin
+ *
+ * @package Rocket\Composer
+ */
 class CmsPlugin implements PluginInterface
 {
-
+    /**
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new CmsInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
 }
